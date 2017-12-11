723cfd0a679cab2910a933a976c6a8464a2cec0b
BielskiGrzegorz <32263838+BielskiGrzegorz@users.noreply.github.com>
2017-12-11 13:42:56 +0100

    Mapa
    mapa i ruch ziom

--- main.cpp
+++ main.cpp
@@ -1,15 +1,75 @@
 #include <iostream>
 #include "Konto.HPP"
+#include "Mapa.HPP"
 #include <string>
+#include <windows.h>
+#include <conio.h>
 
 using namespace std;
+void gotoxy(int x, int y)
+{
+    COORD cord;
+    cord.X = x;
+    cord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cord);
+}
 
 int main()
 {
-    string a="bazaDanych.txt";
+    string a="bazaDanych.txt",b=("mapa.txt");
     Konto Baza;
+    Mapa Jan;
     Baza.wczytKont(a);
     Baza.noweKonto(a);
     Baza.zapisKont(Baza,a);
+    system("cls");
+    Jan.wczytajMape(b);
+    int x=1,y=1;
+     char c='n';
+    for(;;)
+    {
+        Jan.wyswietlMape(1,1);
+        switch (c)
+        {
+        case 'a':
+            {
+                if(Jan.mapa[x][y-1]!='x')
+                y=y-1;
+
+            }
+            break;
+            case 'd':
+            {
+                if(Jan.mapa[x][y+1]!='x')
+                y=y+1;
+
+            }
+            break;
+            case 'w':
+            {
+                if(Jan.mapa[x-1][y]!='x')
+                x=x-1;
+
+            }
+            break;
+             case 's':
+            {
+                if(Jan.mapa[x+1][y]!='x')
+                x=x+1;
+
+            }
+            break;
+        }
+        gotoxy(y,x);
+        cout<<'O';
+
+        c=getch();
+
+
+
+
+        system("cls");
+    }
+
     return 0;
 }
